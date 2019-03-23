19fda23552075e48a4b0ec510b287f3e8ed37668
PanSzelm <wr.swider@gmail.com>
2019-03-23 17:40:07 +0100

    zzz
    z

--- page/main.js
+++ page/main.js
@@ -5,6 +5,10 @@ var vue_app = new Vue({
     el: '#app',
     data: data
 });
+var vue_wrap = new Vue({
+	el:'#another-card',
+	data:{ seen:true }
+})
 function onGamesData(games) {
     data.games = games
 }
