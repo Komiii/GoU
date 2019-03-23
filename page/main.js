0e83b2fae4114bf5e3efa6cdf79e50d2a7fdfa54
Karol Baraniecki <karol@baraniecki.eu>
2019-03-23 18:48:33 +0100

    15572

--- page/main.js
+++ page/main.js
@@ -5,8 +5,25 @@ var data  = {
 };
 var vue_app = new Vue({
     el: '#app',
-    data: data
+    data: data,
+    methods: {
+        gameClicked: function(game) {
+			if(data.seen == game.id) {
+				data.seen = null
+			} else {
+				data.seen = game.id; 
+				setTimeout(function() {
+					setupCanvas(game);
+				}, 0);
+			}
+        }
+    }
 });
+
+function setupCanvas(game) {
+	console.log(game);
+	console.log(document.getElementsByClassName('canvas-picker'));
+}
 
 function onGamesData(games) {
     data.games = games;
