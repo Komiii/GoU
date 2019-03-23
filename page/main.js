ea63b968a03559bf48b0ecdcf43a1438c62420e8
PanSzelm <wr.swider@gmail.com>
2019-03-23 20:54:53 +0100

    z
    z

--- page/main.js
+++ page/main.js
@@ -24,10 +24,16 @@ var GAP_WIDTH = 1080 / 80;
 var CIRCLE_DIAMETER = (1080 - 13 * GAP_WIDTH) / 13;
 var VERTICAL_GAP_HEIGHT = CIRCLE_DIAMETER / 7;
 
-function circle(ctx, x, y) {
+var circles = [];
+var xClick;
+var yClick;
+
+function circle(ctx, x, y, hour, day) {
 	ctx.beginPath();
 	ctx.arc(x + CIRCLE_DIAMETER / 2, y + CIRCLE_DIAMETER / 2, CIRCLE_DIAMETER / 2, 0, 2 * Math.PI);
 	ctx.fill();
+	
+	circles.push([x + CIRCLE_DIAMETER / 2, y + CIRCLE_DIAMETER / 2, hour, day]);
 }
 function text(ctx, text, x, y) {
     ctx.fillStyle = '#fefefe';
@@ -35,10 +41,16 @@ function text(ctx, text, x, y) {
 	ctx.fillText(text, x + CIRCLE_DIAMETER / 2, y + CIRCLE_DIAMETER / 2);
 }
 
+
 var colors = {
 	selected: '#00838f',
 	selectedGap: '#b0bec5',
 }
+
+function compareDist(a,b){
+	return (Math.pow(a[0]-xClick,2)+Math.pow(a[1]-yClick,2))-(Math.pow(b[0]-xClick,2)+Math.pow(b[1]-yClick,2))
+}
+
 
 function setupCanvas(game) {
 	var cnv = document.getElementsByClassName('canvas-picker')[0];
@@ -49,11 +61,18 @@ function setupCanvas(game) {
 		ctx.fillStyle = colors.selected;
 		circle(ctx, 
 			i/2 * (CIRCLE_DIAMETER + GAP_WIDTH) + GAP_WIDTH/2,
-			VERTICAL_GAP_HEIGHT/2);
+			VERTICAL_GAP_HEIGHT/2, i/2, 0); //DODAĆ DZIEŃ
 		text(ctx, i + ':00',
 			i/2 * (CIRCLE_DIAMETER + GAP_WIDTH) + GAP_WIDTH/2,
 			VERTICAL_GAP_HEIGHT/2);
 	}
+	cnv.addEventListener('click',function(event) {
+		xClick=event.pageX;
+		yClick=event.pageY;
+		circles.sort(compareDist);
+		console.log(circles[0]);
+		console.log(xClick,yClick);
+    });
 }
 
 function onGamesData(games) {
