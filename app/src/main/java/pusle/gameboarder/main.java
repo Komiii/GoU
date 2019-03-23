893e7b5e36382944164eb0453d4a6ea24bcba664
PanSzelm <wr.swider@gmail.com>
2019-03-23 12:03:42 +0100

    GH

---
+++ app/src/main/java/pusle/gameboarder/main.java
@@ -0,0 +1,15 @@
+package pusle.gameboarder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class main extends AppCompatActivity {
+
+    public static final String URL = "3.120.189.7:5000";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
