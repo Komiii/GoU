728cee5b6dc9b88e3a7af9795a9e81f3f2f232c2
Karol Baraniecki <karol@baraniecki.eu>
2019-03-23 20:04:36 +0100

    29807

--- page/main.js
+++ page/main.js
@@ -29,6 +29,11 @@ function circle(ctx, x, y) {
 	ctx.arc(x + CIRCLE_DIAMETER / 2, y + CIRCLE_DIAMETER / 2, CIRCLE_DIAMETER / 2, 0, 2 * Math.PI);
 	ctx.fill();
 }
+function text(ctx, text, x, y) {
+    ctx.fillStyle = '#fefefe';
+    ctx.font = "20px Arial";
+	ctx.fillText(text, x + CIRCLE_DIAMETER / 2, y + CIRCLE_DIAMETER / 2);
+}
 
 var colors = {
 	selected: '#00838f',
@@ -39,12 +44,15 @@ function setupCanvas(game) {
 	var cnv = document.getElementsByClassName('canvas-picker')[0];
 	var ctx = cnv.getContext('2d');
 	ctx.textAlign = 'center';
+	ctx.textBaseline = 'middle';
 	for(var i = 0; i <= /* or < ??? */ 24; i += 2) {
 		ctx.fillStyle = colors.selected;
 		circle(ctx, 
 			i/2 * (CIRCLE_DIAMETER + GAP_WIDTH) + GAP_WIDTH/2,
 			VERTICAL_GAP_HEIGHT/2);
-
+		text(ctx, i + ':00',
+			i/2 * (CIRCLE_DIAMETER + GAP_WIDTH) + GAP_WIDTH/2,
+			VERTICAL_GAP_HEIGHT/2);
 	}
 }
 
