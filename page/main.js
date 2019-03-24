6ac7525e5cc135bcdd79e2e035282a59a300a6b0
Karol Baraniecki <karol@baraniecki.eu>
2019-03-24 03:33:35 +0100

    21938

--- page/main.js
+++ page/main.js
@@ -9,14 +9,14 @@ var vue_app = new Vue({
     data: data,
     methods: {
         gameClicked: function(game) {
-			if(data.seen == game.id) {
-				data.seen = null
-			} else {
-				data.seen = game.id; 
-				setTimeout(function() {
-					setupCanvas(game);
-				}, 0);
-			}
+            if(data.seen == game.id) {
+                data.seen = null
+            } else {
+                data.seen = game.id; 
+                setTimeout(function() {
+                    setupCanvas(game);
+                }, 0);
+            }
         }
     }
 });
@@ -28,40 +28,40 @@ var VERTICAL_GAP_HEIGHT = CIRCLE_DIAMETER / 7;
 var circles = [];
 
 function circle(ctx, x, y, hour, day) {
-	ctx.beginPath();
-	ctx.arc(x + CIRCLE_DIAMETER / 2 , y + CIRCLE_DIAMETER / 2 , CIRCLE_DIAMETER / 2 + VERTICAL_GAP_HEIGHT/2, 0, 2 * Math.PI);
-	ctx.fill();
-	
-	circles.push([x + CIRCLE_DIAMETER / 2, y + CIRCLE_DIAMETER / 2, hour, day]);
+    ctx.beginPath();
+    ctx.arc(x + CIRCLE_DIAMETER / 2 , y + CIRCLE_DIAMETER / 2 , CIRCLE_DIAMETER / 2 + VERTICAL_GAP_HEIGHT/2, 0, 2 * Math.PI);
+    ctx.fill();
+
+    circles.push([x + CIRCLE_DIAMETER / 2, y + CIRCLE_DIAMETER / 2, hour, day]);
 }
 
 function rectangle(ctx, x, y, hour, day, ishalf){
-	ctx.beginPath();
-	if(!ishalf){
-	ctx.rect(x - GAP_WIDTH/2 , y - VERTICAL_GAP_HEIGHT/2 , CIRCLE_DIAMETER + GAP_WIDTH, CIRCLE_DIAMETER + VERTICAL_GAP_HEIGHT);
-	ctx.fill();
-	}else{
-	ctx.fillStyle = '#29b6f6';
-	ctx.rect(x - GAP_WIDTH/2 , y - VERTICAL_GAP_HEIGHT/2 , (CIRCLE_DIAMETER + GAP_WIDTH)/2, (CIRCLE_DIAMETER + VERTICAL_GAP_HEIGHT));
-	ctx.fill();
-	ctx.fillStyle = '#00838f';
-	}
-	
-	
-	circles.push([x + CIRCLE_DIAMETER / 2, y + CIRCLE_DIAMETER / 2, hour, day]);
+    ctx.beginPath();
+    if(!ishalf){
+        ctx.rect(x - GAP_WIDTH/2 , y - VERTICAL_GAP_HEIGHT/2 , CIRCLE_DIAMETER + GAP_WIDTH, CIRCLE_DIAMETER + VERTICAL_GAP_HEIGHT);
+        ctx.fill();
+    }else{
+        ctx.fillStyle = '#29b6f6';
+        ctx.rect(x - GAP_WIDTH/2 , y - VERTICAL_GAP_HEIGHT/2 , (CIRCLE_DIAMETER + GAP_WIDTH)/2, (CIRCLE_DIAMETER + VERTICAL_GAP_HEIGHT));
+        ctx.fill();
+        ctx.fillStyle = '#00838f';
+    }
+
+
+    circles.push([x + CIRCLE_DIAMETER / 2, y + CIRCLE_DIAMETER / 2, hour, day]);
 }
 function text(ctx, text, x, y) {
     ctx.font = "20px Arial";
-	ctx.fillText(text, x + CIRCLE_DIAMETER / 2, y + CIRCLE_DIAMETER / 2);
+    ctx.fillText(text, x + CIRCLE_DIAMETER / 2, y + CIRCLE_DIAMETER / 2);
 }
 
 
 var colors = {
-	selectedEnd: {
+    selectedEnd: {
         bg: '#00838f',
         text: '#fefefe',
     },
-	selectedGap: {
+    selectedGap: {
         bg: '#b0bec5',
         text: '#050505',
     },
@@ -125,50 +125,50 @@ function setupCanvas(game, _state) {
     circles = [];
     state = _state || { selectionStep: false, selectionStart: -1, selectionEnd: -1, game: game };
 
-	var cnv = document.getElementsByClassName('canvas-picker')[0];
-	cnv.height = 7*(CIRCLE_DIAMETER+VERTICAL_GAP_HEIGHT);
-	var ctx = cnv.getContext('2d');
-	ctx.textAlign = 'center';
-	ctx.textBaseline = 'middle';
-	for(var j = 0; j<7;j++){
-		text(ctx,days[((new Date().getDay())+j+6)%7], GAP_WIDTH/2,
-				(1/2+j)*VERTICAL_GAP_HEIGHT+j*CIRCLE_DIAMETER);
-		for(var i = 0; i <= /* or < ??? */ 24; i += 2) {
-		    var curColors = getColors(getId(j, i/2));
-		    var curColorsBetween = getColorsBetween(getId(j, i/2), getId(j, i/2) + 1);
-			ctx.fillStyle = curColors.bg;
-				if(getId(j,i/2)==state.selectionStart){
-					if(state.selectionStart!=state.selectionEnd){
-						rectangle(ctx, 
-					(i/2+3/2) * (CIRCLE_DIAMETER + GAP_WIDTH)+GAP_WIDTH/2,
-					(1/2+j)*VERTICAL_GAP_HEIGHT+j*CIRCLE_DIAMETER, i/2, j,1);
-					}
-					circle(ctx, 
-					(i/2+1) * (CIRCLE_DIAMETER + GAP_WIDTH) + GAP_WIDTH/2,
-					(1/2+j)*VERTICAL_GAP_HEIGHT+j*CIRCLE_DIAMETER, i/2, j);
-				}else if(getId(j,i/2)==state.selectionEnd){
-					rectangle(ctx, 
-					(i/2+1) * (CIRCLE_DIAMETER + GAP_WIDTH) + GAP_WIDTH/2,
-					(1/2+j)*VERTICAL_GAP_HEIGHT+j*CIRCLE_DIAMETER, i/2, j,1);
-					circle(ctx, 
-					(i/2+1) * (CIRCLE_DIAMETER + GAP_WIDTH) + GAP_WIDTH/2,
-					(1/2+j)*VERTICAL_GAP_HEIGHT+j*CIRCLE_DIAMETER, i/2, j);
-				}else{
-					rectangle(ctx, 
-					(i/2+1) * (CIRCLE_DIAMETER + GAP_WIDTH) + GAP_WIDTH/2,
-					(1/2+j)*VERTICAL_GAP_HEIGHT+j*CIRCLE_DIAMETER, i/2, j,0);
-				}
-			ctx.fillStyle = curColors.text;
-			text(ctx, i + ':00',
-				(i/2+1) * (CIRCLE_DIAMETER + GAP_WIDTH) + GAP_WIDTH/2,
-				(1/2+j)*VERTICAL_GAP_HEIGHT+j*CIRCLE_DIAMETER);
+    var cnv = document.getElementsByClassName('canvas-picker')[0];
+    cnv.height = 7*(CIRCLE_DIAMETER+VERTICAL_GAP_HEIGHT);
+    var ctx = cnv.getContext('2d');
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'middle';
+    for(var j = 0; j<7;j++){
+        text(ctx,days[((new Date().getDay())+j+6)%7], GAP_WIDTH/2,
+            (1/2+j)*VERTICAL_GAP_HEIGHT+j*CIRCLE_DIAMETER);
+        for(var i = 0; i <= /* or < ??? */ 24; i += 2) {
+            var curColors = getColors(getId(j, i/2));
+            var curColorsBetween = getColorsBetween(getId(j, i/2), getId(j, i/2) + 1);
+            ctx.fillStyle = curColors.bg;
+            if(getId(j,i/2)==state.selectionStart){
+                if(state.selectionStart!=state.selectionEnd){
+                    rectangle(ctx, 
+                        (i/2+3/2) * (CIRCLE_DIAMETER + GAP_WIDTH)+GAP_WIDTH/2,
+                        (1/2+j)*VERTICAL_GAP_HEIGHT+j*CIRCLE_DIAMETER, i/2, j,1);
+                }
+                circle(ctx, 
+                    (i/2+1) * (CIRCLE_DIAMETER + GAP_WIDTH) + GAP_WIDTH/2,
+                    (1/2+j)*VERTICAL_GAP_HEIGHT+j*CIRCLE_DIAMETER, i/2, j);
+            }else if(getId(j,i/2)==state.selectionEnd){
+                rectangle(ctx, 
+                    (i/2+1) * (CIRCLE_DIAMETER + GAP_WIDTH) + GAP_WIDTH/2,
+                    (1/2+j)*VERTICAL_GAP_HEIGHT+j*CIRCLE_DIAMETER, i/2, j,1);
+                circle(ctx, 
+                    (i/2+1) * (CIRCLE_DIAMETER + GAP_WIDTH) + GAP_WIDTH/2,
+                    (1/2+j)*VERTICAL_GAP_HEIGHT+j*CIRCLE_DIAMETER, i/2, j);
+            }else{
+                rectangle(ctx, 
+                    (i/2+1) * (CIRCLE_DIAMETER + GAP_WIDTH) + GAP_WIDTH/2,
+                    (1/2+j)*VERTICAL_GAP_HEIGHT+j*CIRCLE_DIAMETER, i/2, j,0);
+            }
+            ctx.fillStyle = curColors.text;
+            text(ctx, i + ':00',
+                (i/2+1) * (CIRCLE_DIAMETER + GAP_WIDTH) + GAP_WIDTH/2,
+                (1/2+j)*VERTICAL_GAP_HEIGHT+j*CIRCLE_DIAMETER);
 
 
-			// for next text() above this loop
-			ctx.fillStyle = curColorsBetween.text;
-		}
-	}
-	cnv.onclick = function(event) {
+            // for next text() above this loop
+            ctx.fillStyle = curColorsBetween.text;
+        }
+    }
+    cnv.onclick = function(event) {
         var b = cnv.getBoundingClientRect();
         var scale = cnv.width / parseFloat(b.width);
         var xClick = (event.clientX - b.left) * scale;
@@ -176,8 +176,8 @@ function setupCanvas(game, _state) {
         function compareDist(a, b) {
             return (Math.pow(a[0]-xClick,2)+Math.pow(a[1]-yClick,2))-(Math.pow(b[0]-xClick,2)+Math.pow(b[1]-yClick,2))
         }
-		circles.sort(compareDist);
-		onClick(circles[0][2], circles[0][3]);
+        circles.sort(compareDist);
+        onClick(circles[0][2], circles[0][3]);
     };
 }
 
